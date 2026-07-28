290a9450b7a92bf6cc049e542de1c91cf5144960
Júlia Hartmann <juliahartmann@outlook.com.br>
2026-07-28 14:28:48 -0300

    Adiciona TestCleanup

--- tests/GeradorDeProvas.Testes.Integracao/ModuloProva/RepositorioProvaEmOrmTests.cs
+++ tests/GeradorDeProvas.Testes.Integracao/ModuloProva/RepositorioProvaEmOrmTests.cs
@@ -15,12 +15,20 @@ public sealed class RepositorioProvaOrmTests
     private GeradorDeProvasDbContext dbContext = null!;
     private RepositorioProvaEmOrm repositorio = null!;
 
+    // Hooks / Ganchos
     [TestInitialize]
     public void InicializarRepositorio()
     {
         dbContext = CriarDbContext(Guid.NewGuid());
 
         repositorio = new RepositorioProvaEmOrm(dbContext);
+    }
+
+    [TestCleanup]
+    public void LimparContexto()
+    {
+        dbContext.Dispose();
+
     }
 
     [TestMethod]
